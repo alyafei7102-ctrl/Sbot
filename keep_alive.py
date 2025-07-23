7f5c7e05ee00c13e2c8805ccab2669d3adbec296
alyafei7102-ctrl <alyafei7102@gmail.com>
2025-07-23 22:24:42 +0300

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -5,7 +5,7 @@ app = Flask('')
 
 @app.route('/')
 def home():
-    return "البوت شغال ✅ من Replit"
+    return "✅ البوت شغال على Replit ومرتبط بـ UptimeRobot"
 
 def run():
     app.run(host='0.0.0.0', port=8080)
